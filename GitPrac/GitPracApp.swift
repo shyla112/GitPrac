1ee2cd5b7e2289dba7691dbfe429e67e18195d50
Shyla <shyla9557@gmail.com>
2023-07-14 10:24:49 -0400

    Initial Commit

---
+++ GitPrac/GitPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracApp.swift
+//  GitPrac
+//
+//  Created by Shyla Budhai :) on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GitPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
